4e50e31293a735963dee5a2dab84f2c64fb49c77
Soumya Deb <debloper@gmail.com>
2015-08-22 21:52:39 +0530

    Cleaned up, alternative *bare-minimum* text streaming with event-stream

--- test/text.js
+++ test/text.js
@@ -1,9 +1,12 @@
 var http = require('http')
-  , file = require('fs');
+  , file = require('fs')
+  , strm = require('event-stream');
 
 http.createServer(function(req, res) {
 
-  file.createReadStream(__dirname + '/data/oneRing.txt').pipe(res);
-  file.createReadStream(__dirname + '/data/Ozymandias.txt').pipe(res);
+  strm.merge([
+    file.createReadStream(__dirname + '/data/oneRing.txt'),
+    file.createReadStream(__dirname + '/data/Ozymandias.txt')
+  ]).pipe(res);
 
 }).listen(8080);
